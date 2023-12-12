9131981ae38269cb8a41f74b605095d8d9f9c432
Kaushal-Kishore-Gupta <thekaushalkishoregupta@gmail.com>
2023-12-12 22:24:27 +0530

    added some performamce metrics not tested

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from .serializers import *
 from .models import *
 from rest_framework import status
-
+from datetime import datetime
 
 
 
@@ -18,6 +18,7 @@ class Vendors(APIView):
         serializer = vendorSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
+            HistoricalPerformance.objects.create(vendor=serializer.instance)
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     
@@ -73,16 +74,59 @@ class PurchaseOrderDetailView(APIView):
         serializer = purchaseOrderSerializer(purchase_order)
         return Response(serializer.data)
     
-    def put(self,request,po_id):
-        try:
-            purchase_order = PurchaseOrder.objects.get(id=po_id)
-        except PurchaseOrder.DoesNotExist:
+    def calculate_on_time_delivery_rate(self, purchase_order):
+        new_status = purchase_order.status
+        total_completed_orders = PurchaseOrder.objects.filter(vendor=purchase_order.vendor, status='completed').count()
+        old_on_time_delivery_rate = purchase_order.vendor.on_time_delivery_rate
+
+        if new_status == 'completed':
+            if datetime.now() <= purchase_order.delivery_date:
+                new_on_time_delivery_rate = (old_on_time_delivery_rate * total_completed_orders + 1) / (total_completed_orders + 1)
+            else:
+                new_on_time_delivery_rate = old_on_time_delivery_rate * total_completed_orders / (total_completed_orders + 1)
+            
+            purchase_order.vendor.on_time_delivery_rate = new_on_time_delivery_rate
+            purchase_order.vendor.save()
+
+            historical_performance, created = HistoricalPerformance.objects.get_or_create(vendor=purchase_order.vendor)
+            historical_performance.on_time_delivery_rate = new_on_time_delivery_rate
+            historical_performance.date = datetime.now()
+            historical_performance.save()
+
+    def calculate_quality_rating_avg(self, purchase_order, quality_rating):
+        old_quality_rating_avg = purchase_order.vendor.quality_rating_avg
+        total_ratings=PurchaseOrder.objects.filter(vendor=purchase_order.vendor).exclude(quality_rating=None).count()
+        new_quality_rating_avg = (old_quality_rating_avg * total_ratings + quality_rating) / (total_ratings + 1)
+        purchase_order.vendor.quality_rating_avg = new_quality_rating_avg
+        purchase_order.vendor.save()
+        HistoricalPerformance.objects.filter(vendor=purchase_order.vendor).update(quality_rating_avg=new_quality_rating_avg)
+
+    
+    def put(self, request, po_id):
+        purchase_order = self.get_purchase_order(po_id)
+
+        if not purchase_order:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = purchaseOrderSerializer(purchase_order,data=request.data)
+        
+        serializer = PurchaseOrderSerializer(purchase_order, data=request.data)
+
         if serializer.is_valid():
+            old_status = purchase_order.status
             serializer.save()
+
+            new_status = serializer.validated_data.get('status', old_status)
+
+            if old_status != 'completed' and new_status == 'completed':
+                self.calculate_on_time_delivery_rate(purchase_order)
+
+            #check if quality rating is present
+            quality_rating = serializer.validated_data.get('quality_rating', None)
+            if quality_rating:
+                self.calculate_quality_rating_avg(purchase_order, quality_rating)
+
             return Response(serializer.data)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def delete(self,request,po_id):
         try:
@@ -95,3 +139,6 @@ class PurchaseOrderDetailView(APIView):
 
 class VendorPerformance(APIView):
     pass
+
+
+
